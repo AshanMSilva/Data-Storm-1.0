79080d3f8d05ddff751f398f7cf39dbdfeb1dfab
Ashan M Silva <47840176+AshanMSilva@users.noreply.github.com>
2020-02-14 16:21:33 +0530

    Update version 1.py
    label encoder used

--- version 1.py
+++ version 1.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Feb 14 06:07:34 2020
+Created on Fri Feb 14 10:16:02 2020
 
 @author: user
 """
@@ -8,76 +8,61 @@ Created on Fri Feb 14 06:07:34 2020
 
 import pandas as pd
 import numpy as np
-
+import tensorflow as tf
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
 from tensorflow.keras.callbacks import TensorBoard
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+training_data = (pd.read_csv('credit_card_default_train.csv')) #load train csv file and convert it to a 2D matrix
+test_data = (pd.read_csv('credit_card_default_test.csv')) #load test csv file and convert it to a 2D matrix
+training_data.head()
+test_data.head()
+labels = training_data.iloc[:,-1].values
+features =training_data.iloc[:,1:-1].values
+x_test =test_data.iloc[:,1:].values
+
+#imputer = Imputer(missing_values='NaN', strategy='mean', axis=0)
 
 
-training_data = np.array(pd.read_csv('credit_card_default_train.csv')) #load train csv file and convert it to a 2D matrix
-test_data = np.array(pd.read_csv('credit_card_default_test.csv')) #load test csv file and convert it to a 2D matrix
 
-y_train=training_data[:,-1] #get the y values from training_data
+encode= LabelEncoder()
+features[:,0] = encode.fit_transform(features[:,0])
+features[:,1] = encode.fit_transform(features[:,1])
+features[:,2] = encode.fit_transform(features[:,2])
+features[:,3] = encode.fit_transform(features[:,3])
+features[:,4] = encode.fit_transform(features[:,4])
 
-#print(y_train)
+x_test[:,0] = encode.fit_transform(x_test[:,0])
+x_test[:,1] = encode.fit_transform(x_test[:,1])
+x_test[:,2] = encode.fit_transform(x_test[:,2])
+x_test[:,3] = encode.fit_transform(x_test[:,3])
+x_test[:,4] = encode.fit_transform(x_test[:,4])
+#print(features[:,2])
+x_test= x_test.astype(np.float)
+#print(x_train.iloc[0,:]['Gender'])
+#print(features)
+print(x_test.shape)
+print(features.shape)
+features = features.astype(np.float)
+#x_train, x_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=1)
 
-drop_cols=[0,24] #colomns that should remove from x
-x_train =np.delete(training_data,drop_cols,axis=1) #get x_train matrix
-
-x_test = np.delete(test_data,[0],axis=1) #get x_test matrix
-
-x_train_length =x_train.shape
-y_train_length =y_train.shape
-x_test_length =x_test.shape
-
-#print(x_test_length);
-#print(x_train_length);
-#print(y_train_length);
-
-i=0
-
-for i in range(x_train_length[0]):
-    if(type(x_train[i][0])==type("a")):
-        if(x_train[i][0][-1]=='M'):
-             x_train[i][0]= float(x_train[i][0][:-1])*10**6
-        elif(x_train[i][0][-1]=='K' or x_train[i][0][-1]=='k'):
-             x_train[i][0]= float(x_train[i][0][:-1])*10**3
-    if(x_train[i][1]=="M"):
-        x_train[i][1]= 1
-    if(x_train[i][1]=="F"):
-        x_train[i][1]= 0
-    if(x_train[i][2]=="Other"):
-        x_train[i][2]= 0
-    if(x_train[i][2]=="Graduate"):
-        x_train[i][2]= 1
-    if(x_train[i][2]=="High School"):
-        x_train[i][2]= 2
-    if(x_train[i][3]=="Other"):
-        x_train[i][3]= 0
-    if(x_train[i][3]=="Single"):
-        x_train[i][3]= 1
-    if(x_train[i][3]=="Single"):
-        x_train[i][3]= 1
-    if(x_train[i][4]=="More than 65"): #479
-        x_train[i][4]= 3
-    if(x_train[i][4]=="46-65"):
-        x_train[i][4]= 2
-    if(x_train[i][4]=="31-45"):
-        x_train[i][4]= 1
-    if(x_train[i][4]=="Less than 30"):
-        x_train[i][4]= 0  
-
-
-x_train = x_train.astype(np.float)
-#x_test = x_test.astype(np.float)
-X_train, X_test, Y_train, Y_test = train_test_split(x_train, y_train, test_size=0.3, random_state=42)
-
-
-X_train_length =X_train.shape
-
-
-model= Sequential([Flatten(),Dense(16,activation='sigmoid',input_shape=X_train_length),Dense(16,activation='sigmoid'),Dense(1,activation='sigmoid')])
+model= Sequential([Flatten(),Dense(16,activation='sigmoid',input_shape=features.shape),Dense(16,activation='sigmoid'),Dense(1,activation='sigmoid')])
 model.compile(optimizer='adam', loss='mean_squared_error',metrics=['accuracy'])
-model.fit(X_train,Y_train,validation_data=(X_test,Y_test), epochs=10, verbose = 1)
+model.fit(features, labels, epochs=2, verbose = 1)
+prediction = model.predict(x_test)
+print(model.predict(x_test))
+
+results =np.zeros(prediction.shape)
+j=0
+for j in range(prediction.shape[0]):
+    if(prediction[j]<0.5):
+        results[j]=0
+    else:
+        results[j] =1
+
+print(results)
