77a6b90fdcefaf7b5fd8ac69590cedba47da8c8d
VimuthR <vimuth.17@cse.mrt.ac.lk>
2020-02-14 18:53:00 +0530

    Update normalize.py

--- Day 01/normalize.py
+++ Day 01/normalize.py
@@ -1,9 +1,9 @@
 def normalize(x_train):
     for column in range(11,23):
-        avg = sum(x_train[:,column])/24000
+        avg = sum(x_train[:,column])/x_train.shape[0]
         #print (avg)
         datarange = (max(x_train[:,column]) - min(x_train[:,column]))
         #print(datarange)
-        for data in range(0,24000):
+        for data in range(0,x_train.shape[0]):
             x_train[data,column] = (x_train[data,column] - avg)/datarange
     return x_train
